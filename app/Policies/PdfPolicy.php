6da74c618205f56699c78cb95d4811ed21489dc0
abdullatef trefi <abdullatef.trefi98@gmail.com>
2023-03-02 10:33:32 +0400

    d

---
+++ app/Policies/PdfPolicy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Pdf;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PdfPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Pdf  $pdf
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Pdf $pdf)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Pdf  $pdf
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Pdf $pdf)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Pdf  $pdf
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Pdf $pdf)
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Pdf  $pdf
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Pdf $pdf)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Pdf  $pdf
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Pdf $pdf)
+    {
+        //
+    }
+}
